08ff94c3ee17c1c3e4452f091bb1f400fd23d745
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2019-11-27 08:24:38 -0500

    added database for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Che99chi01",
-  database: "burgers_db"
+  user: "if0w6wa3lb8mpxr6",
+  password: "vb3fc6egvtm2gjzk",
+  database: "	lt1htnvnd40g6enk"
 });
+"test":{
+  host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "if0w6wa3lb8mpxr6",
+  password: "vb3fc6egvtm2gjzk",
+  database: "	lt1htnvnd40g6enk"
 
+}
+"production":{
+  host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "if0w6wa3lb8mpxr6",
+  password: "vb3fc6egvtm2gjzk",
+  database: "	lt1htnvnd40g6enk"
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
