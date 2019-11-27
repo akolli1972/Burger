0cd41b91c7a6649b1633f6361716f2257aa34233
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2019-11-27 09:25:25 -0500

    made changes to databast table

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,27 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "127.0.0.1",
   port: 3306,
-  user: "if0w6wa3lb8mpxr6",
-  password: "vb3fc6egvtm2gjzk",
-  database: "	lt1htnvnd40g6enk"
+  user: "root",
+  password: "Che99chi01",
+  database: "burgers_db"
 });
-"test":{
-  host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "if0w6wa3lb8mpxr6",
-  password: "vb3fc6egvtm2gjzk",
-  database: "	lt1htnvnd40g6enk"
+// "test":{
+//   host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "if0w6wa3lb8mpxr6",
+//   password: "vb3fc6egvtm2gjzk",
+//   database: "	lt1htnvnd40g6enk"
 
-}
-"production":{
-  host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "if0w6wa3lb8mpxr6",
-  password: "vb3fc6egvtm2gjzk",
-  database: "	lt1htnvnd40g6enk"
-};
+// }
+// "production":{
+//   host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "if0w6wa3lb8mpxr6",
+//   password: "vb3fc6egvtm2gjzk",
+//   database: "	lt1htnvnd40g6enk"
+// };
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
