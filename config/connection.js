29edef8cfb01fc388b6956567060f911de6c72d2
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2019-11-29 20:29:37 -0500

    added changes to deploy on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,30 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "Che99chi01",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    // port: 3306,
+    user: "if0w6wa3lb8mpxr6",
+    password: "vb3fc6egvtm2gjzk",
+    database: "lt1htnvnd40g6enk"
+  });
+}
+//   host: "127.0.0.1",
+//   port: 3306,
+//   user: "root",
+//   password: "Che99chi01",
+//   database: "burgers_db"
+// });
 // "test":{
-//   host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   host: "kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 //   port: 3306,
 //   user: "if0w6wa3lb8mpxr6",
 //   password: "vb3fc6egvtm2gjzk",
-//   database: "	lt1htnvnd40g6enk"
-
+//   database: "lt1htnvnd40g6enk"
+// });
 // }
 // "production":{
 //   host: "	kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
